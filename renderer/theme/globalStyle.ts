909f2540399f0e7dc01f2235b7b75f7fc3ae0f12
Futhememe <kyoro_souza@hotmail.com>
2022-10-08 19:56:35 -0300

    💄 update global styles and theme

--- renderer/theme/globalStyle.ts
+++ renderer/theme/globalStyle.ts
@@ -4,6 +4,34 @@ export const GlobalStyle = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
-    font-family: sans-serif;
+    box-sizing: border-box;
+    outline: 0;
+    color: ${props => props.theme.colors.dark[500]};
+  }
+  body {
+    background: transparent;
+    -webkit-font-smoothing: antialiased;
+  }
+  body, input, button {
+    font: 400 1rem Roboto, sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+  
+  ::-webkit-scrollbar {
+    width: 8px;
+    border-radius: 4px;
+    background: rgba(255, 255, 255, 0.05);
+  }
+  
+  ::-webkit-scrollbar-track {
+    border-radius: 4px;
+  }
+  
+  ::-webkit-scrollbar-thumb {
+    background: rgba(255, 255, 255, 0.1);
+    border-radius: 4px;
   }
 `
---
+++ renderer/theme/theme.ts
@@ -0,0 +1,13 @@
+export const defaultTheme = {
+  colors: {
+    primary: {
+      300: '#66DF9C'
+    },
+    gray: {
+      100: '#717171'
+    },
+    dark: {
+      500: '#131313'
+    }
+  },
+}
